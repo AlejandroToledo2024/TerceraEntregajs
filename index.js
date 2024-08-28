2b3f2ed1a79c0fd38f6efdc9ed938ec4fa2a1e6c
AlejandroToledo2024 <alyo20111@gmail.com>
2024-08-27 21:22:51 -0300

    agregar al carrito funcional

--- index.js
+++ index.js
@@ -4,55 +4,63 @@ const cardData = [
     Nombre: "Producto1",
     descripcion: "Descripción tarjeta 1",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 1500,
   },
   {
     id: 2,
     Nombre: "Producto2",
     descripcion: "Descripción tarjeta 2",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 2000,
   },
   {
     id: 3,
     Nombre: "Producto3",
     descripcion: "Descripción tarjeta 3",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 2500,
   },
   {
     id: 4,
     Nombre: "Producto4",
     descripcion: "Descripción tarjeta 4",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 3000,
   },
   {
     id: 5,
-    Nombre: "Producto4",
+    Nombre: "Producto5",
     descripcion: "Descripción tarjeta 4",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 3500,
   },
   {
     id: 6,
-    Nombre: "Producto4",
+    Nombre: "Producto6",
     descripcion: "Descripción tarjeta 4",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 4000,
   },
   {
     id: 7,
-    Nombre: "Producto4",
+    Nombre: "Producto7",
     descripcion: "Descripción tarjeta 4",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 4500,
   },
   {
     id: 8,
-    Nombre: "Producto4",
+    Nombre: "Producto8",
     descripcion: "Descripción tarjeta 4",
     imageUrl: "https://via.placeholder.com/200",
+    precio: 5000,
   },
 ];
+
 document.addEventListener("DOMContentLoaded", function () {
   const contenedorDeCartas = document.getElementById("contenedorDeCartas");
 
-
-  function createCard(nombre, descripcion, imageUrl) {
+  function createCard(nombre, descripcion, imageUrl, id , precio) {
     const card = document.createElement("div");
     card.className = "card";
 
@@ -66,38 +74,39 @@ document.addEventListener("DOMContentLoaded", function () {
     const cardDescription = document.createElement("p");
     cardDescription.textContent = descripcion;
 
-    const botonAdd = document.createElement("button");
-    botonAdd.textContent = "Agregar al carrito";    
-    botonAdd.id = 'botonAdd'
-    botonAdd.classList = 'nuevoBtn'
-
     card.appendChild(cardImage);
     card.appendChild(cardTitle);
     card.appendChild(cardDescription);
-    card.appendChild(botonAdd);
 
     return card;
   }
+  cardData.forEach((data ) => {
+    const card = createCard(
+      data.Nombre,
+      data.descripcion,
+      data.imageUrl,
+      
+    );
+    const botonAdd = document.createElement("button");
+    botonAdd.textContent = "Agregar al carrito";
+    botonAdd.classList = "nuevoBtn";
+    botonAdd.id = data.id;
 
-  cardData.forEach((data) => {
-    const card = createCard(data.Nombre, data.descripcion, data.imageUrl);
-
+    card.appendChild(botonAdd);
     contenedorDeCartas.appendChild(card);
+    botonAdd.addEventListener ( 'click' , () => {
+      const product = data;
+      carrito.push({
+        nombre:product.Nombre,
+        precio:product.precio,
+        descripcion:product.descripcion
+
+      })
+      localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    } )
   });
 });
-// document.querySelectorAll('.button').forEach(button => {
-//     button.addEventListener('click', () => {
-//         const idProducto = Event.AT_TARGET.getAttribute('data-id'); 
-//         agregarAlCarrito(idProducto);
-// });
-// function agregarAlCarrito(idProducto){
-//     let carrito = JSON.parse(localStorage.getItem ('carrito')) || [] ;
-//     if (!carrito.includes(idProducto)){
-//         carrito.push(idProducto);
-//         localStorage.setItem('carrito', JSON.stringify(carrito));
-//     } 
-//     }
-// });
 
 let modo = localStorage.getItem("Modo");
 
@@ -139,3 +148,9 @@ document.addEventListener("DOMContentLoaded", function () {
     cambiarTexto();
   });
 });
+
+let carrito = [];
+
+const botones = document.querySelectorAll('.nuevoBtn');
+
+botones
