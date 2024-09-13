718b1c9310bd516b03fe67a6090a0b65c7092e37
AlejandroToledo2024 <alyo20111@gmail.com>
2024-09-13 17:22:53 -0300

    Modificacion de Nombres para corregir errores y fetch de productos

--- js/productosfetch.js
+++ js/productosfetch.js
@@ -1,5 +1,19 @@
-const cardContenedor = document.getElementById("contenedorDeCartas")
+const cardContenedor = document.getElementById("contenedorDeCartas");
 
-fetch("/productos.json")
-.then((response)=> response.json())
-.then((data)=> console.log(data));
+fetch("../productos.json")
+  .then((response) => response.json())
+  .then((data) => {
+    for (item of data){
+      const card = document.createElement("div");
+      card.innerHTML = `
+      <div class="card">
+      <img src= ${item.imageUrl} alt="Imagen del producto">
+      <h2>${item.Nombre}</h2>
+      <p>${item.descripcion}</p>
+      <p>$${item.precio}</p>
+      <button class="nuevoBtn" id="1">Agregar al carrito</button>
+      </div>
+      `;
+      cardContenedor.appendChild(card);
+    };
+  });
